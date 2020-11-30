f126d3723c93d89110c2bf5288dc16aa4aa089dd
Filip Komogorov <filipkomogorov@gmail.com>
2020-11-30 23:36:10 +0100

    Events section completed for Desktop size

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,10 +17,65 @@ var navigation = document.getElementById("nav");
 //  EVENTS 
 // ======================================
 
+// EVENT-1
+
+document.getElementById('eventOneLeft').addEventListener('click', function () {
+    document.getElementById('infoOneEventLeft').classList.toggle('grid-show');
+    document.getElementById('card-one-left').classList.toggle('card-expanded');
+    document.getElementById('lineOneLeft').classList.toggle('line-animation');
+})
+
+document.getElementById('eventOneRight').addEventListener('click', function () {
+    document.getElementById('infoOneEventRight').classList.toggle('grid-show');
+    document.getElementById('card-one-right').classList.toggle('card-expanded');
+    document.getElementById('lineOneRight').classList.toggle('line-animation');
+})
+
+// EVENT-2
+
+document.getElementById('eventTwoLeft').addEventListener('click', function () {
+    document.getElementById('infoTwoEventLeft').classList.toggle('grid-show');
+    document.getElementById('card-two-left').classList.toggle('card-expanded');
+    document.getElementById('lineTwoLeft').classList.toggle('line-animation');
+})
+
+document.getElementById('eventTwoRight').addEventListener('click', function () {
+    document.getElementById('infoTwoEventRight').classList.toggle('grid-show');
+    document.getElementById('card-two-right').classList.toggle('card-expanded');
+    document.getElementById('lineTwoRight').classList.toggle('line-animation');
+})
+
+// EVENT-3
+
+document.getElementById('eventThreeLeft').addEventListener('click', function () {
+    document.getElementById('infoThreeEventLeft').classList.toggle('grid-show');
+    document.getElementById('card-three-left').classList.toggle('card-expanded');
+    document.getElementById('lineThreeLeft').classList.toggle('line-animation');
+})
+
+document.getElementById('eventThreeRight').addEventListener('click', function () {
+    document.getElementById('infoThreeEventRight').classList.toggle('grid-show');
+    document.getElementById('card-three-right').classList.toggle('card-expanded');
+    document.getElementById('lineThreeRight').classList.toggle('line-animation');
+})
+
+// EVENT-4
+
+document.getElementById('eventFourLeft').addEventListener('click', function () {
+    document.getElementById('infoFourEventLeft').classList.toggle('grid-show');
+    document.getElementById('card-four-left').classList.toggle('card-expanded');
+    document.getElementById('lineFourLeft').classList.toggle('line-animation');
+})
+
+document.getElementById('eventFourRight').addEventListener('click', function () {
+    document.getElementById('infoFourEventRight').classList.toggle('grid-show');
+    document.getElementById('card-four-right').classList.toggle('card-expanded');
+    document.getElementById('lineFourRight').classList.toggle('line-animation');
+})
 
 
-var eventOneLeft = document.getElementById('eventOneLeft');
-var eventOneRight = document.getElementById('eventOneRight');
+
+
 
 // ======================================
 //  EVENT LISTENERS
@@ -56,15 +111,5 @@ function expandOneLeft() {
     infoOneEventLeft.classList.toggle('grid-show')
 }
 
-document.getElementById('eventOneLeft').addEventListener('click', function () {
-    document.getElementById('infoOneEventLeft').classList.toggle('grid-show');
-    document.getElementById('card-one-left').classList.toggle('card-expanded');
-    document.getElementById('lineOneLeft').classList.toggle('line-animation');
-})
 
-document.getElementById('eventOneRight').addEventListener('click', function () {
-    document.getElementById('infoOneEventRight').classList.toggle('grid-show');
-    document.getElementById('card-one-right').classList.toggle('card-expanded');
-    document.getElementById('lineOneRight').classList.toggle('line-animation');
-})
 
